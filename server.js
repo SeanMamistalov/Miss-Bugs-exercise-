7b060be08cacf99a6c6bf91d48852e4ef90b6e43
Sean <mamistalovsean@gmail.com>
2024-06-15 16:28:21 +0300

    Add save and remove bug

--- server.js
+++ server.js
@@ -16,12 +16,33 @@ app.get("/api/bug", (req, res) => {
   res.send(bugs);
 });
 
-app.get("/api/bug/save", (req, res) => {});
-app.get("/api/bug/:bugId", (req, res) => {
+app.get("/api/bug/save", (req, res) => {
+    console.log(req.query)
+  const { _id, name } = req.query;
+  const bugToSave = { _id, name };
+
+  if (_id) {
+    const idx = bugs.findIndex((bug) => bug._id === _id);
+    bugs.splice(idx, 1, bugToSave);
+  } else {
+    bugToSave._id = "b" + (Date.now() % 1000);
+    bugs.push(bugToSave);
+  }
+  res.send(bugToSave);
+});
+
+app.get("/api/bug/:id", (req, res) => {
   const { id } = req.params;
-  const bug = bugs.find((bug) => bug.id === id);
-  
+  const bug = bugs.find((bug) => bug._id === id);
+
   res.send(bug);
 });
 
-app.get("/api/bug/:bugId/remove", (req, res) => {});
+app.get("/api/bug/:id/remove", (req, res) => {
+  const { id } = req.params;
+  const idx = bugs.findIndex((bug) => bug._id === id);
+
+  bugs.splice(idx, 1);
+
+  res.send(`Bug ${id} has been removed...`);
+});
