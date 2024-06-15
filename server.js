4793877a4341236d222ad2e04b3612f23cf2442a
Sean <mamistalovsean@gmail.com>
2024-06-15 15:33:11 +0300

    Update app - bug & bugId

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-import express from 'express'
-const app = express()
-app.get('/', (req, res) => res.send('Hello There'))
-app.listen(3030, () => console.log('Server ready at port http://127.0.0.1:3030/'))
+import express from "express";
+const app = express();
+app.get("/", (req, res) => res.send("Hello There"));
+app.listen(3030, () =>
+  console.log("Server ready at port http://127.0.0.1:3030/")
+);
 
+var bugs = [
+  { _id: "b101", name: "hishi" },
+  { _id: "b102", name: "hipushit" },
+  { _id: "b103", name: "dvora" },
+  { _id: "b104", name: "dabur" },
+];
 
-app.get('/bug', (req, res) => {
-    res.send('<h1>Hello Bugs!<h1>')
-})
-app.get('/api/bug/save', (req, res) => {})
-app.get('/api/bug/:bugId', (req, res) => {})
-app.get('/api/bug/:bugId/remove', (req, res) => {})
+app.get("/api/bug", (req, res) => {
+  res.send(bugs);
+});
+
+app.get("/api/bug/save", (req, res) => {});
+app.get("/api/bug/:bugId", (req, res) => {
+  const { id } = req.params;
+  const bug = bugs.find((bug) => bug.id === id);
+  
+  res.send(bug);
+});
+
+app.get("/api/bug/:bugId/remove", (req, res) => {});
