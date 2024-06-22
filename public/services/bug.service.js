4d6505d5646e69fb40b2edab39cc86546a62d4d7
Sean <mamistalovsean@gmail.com>
2024-06-22 20:08:00 +0300

    Update bug service

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -1,30 +1,54 @@
-
-import { storageService } from './async-storage.service.js'
-import { utilService } from './util.service.js'
-
-const STORAGE_KEY = 'bugDB'
-
+const BASE_URL = "/api/bug";
 export const bugService = {
-    query,
-    getById,
-    save,
-    remove,
-}
+  query,
+  getById,
+  save,
+  remove,
+  createDefaultFilter,
+  onDownloadPdf,
+};
 
-
-function query() {
-    return storageService.query(STORAGE_KEY)
+function query(filterBy) {
+  const { txt, minSeverity = +minSeverity } = filterBy;
+  console.log("🚀 ~ query ~ minSeverity:", minSeverity);
+  console.log("🚀 ~ query ~ txt:", txt);
+  return axios
+    .get(`${BASE_URL}?minSeverity=${minSeverity}&txt=${txt}`)
+    .then((res) => res.data);
 }
 function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
+  return axios
+    .get(BASE_URL + `/${bugId}`)
+    .then((res) => res.data)
+    .catch(console.log);
 }
 function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+  return axios.get(BASE_URL + `/${bugId}/remove`).then((res) => res.data);
 }
 function save(bug) {
-    if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
-    }
+  const { _id, description, title, createdAt, severity } = bug;
+  if (bug._id) {
+    return axios
+      .get(
+        BASE_URL +
+          `/save?_id=${_id}&severity=${severity}&title=${title}&createdAt=${createdAt}&description=${description}`
+      )
+      .then((res) => res.data);
+  } else {
+    return axios
+      .get(
+        BASE_URL +
+          `/save?severity=${severity}&title=${title}&createdAt=${createdAt}&description=${description}`
+      )
+      .then((res) => res.data);
+  }
+}
+
+function createDefaultFilter() {
+  return { txt: "", minSeverity: 0 };
+}
+
+function onDownloadPdf() {
+  console.log("in front download");
+  return axios.get(BASE_URL + "/download").then((res) => res.data);
 }
